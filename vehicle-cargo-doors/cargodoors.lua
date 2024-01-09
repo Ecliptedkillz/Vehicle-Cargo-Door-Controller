92fe31fec887172f65bbbcde66f7cc2f85a1d95c
Ecliptedkillz <65789295+Ecliptedkillz@users.noreply.github.com>
2024-01-09 11:51:36 -0500

    Update cargodoors.lua

--- vehicle-cargo-doors/cargodoors.lua
+++ vehicle-cargo-doors/cargodoors.lua
@@ -1,11 +1,14 @@
-RegisterKeyMapping("cargodoor", "Toggle Cargo Doors", "keyboard", "Y")
-
 -- Config
-local commandName = "cargodoor" -- Command
-local vehicleWhitelist = {"cv22b", "mh47g", "mh60k", "uh60v", "f14a", "f14d2", "ch47d"} -- Vehicle Whitelist
+RegisterKeyMapping("cargodoor", "Toggle Cargo Doors", "keyboard", "Y")
+RegisterKeyMapping("bombbay", "Toggle Bomb Bays", "keyboard", "B")
+local doorCommandName = "cargodoor" -- Command for cargo doors
+local bombBayCommandName = "bombbay" -- Command for bomb bays
+local vehicleWhitelist = {"cv22b", "mh47g", "mh60k", "uh60v", "f14a", "f14d2", "ch47d"} -- Cargo Door Whitelist
+local bombBayWhitelist = {"bombushka", "f22a"} -- Bomb bay whitelist
 
-local function isVehicleWhitelisted(vehicleName)
-    for _, whitelistedVehicle in ipairs(vehicleWhitelist) do
+-- Function to check if a vehicle is whitelisted
+local function isVehicleWhitelisted(vehicleName, whitelist)
+    for _, whitelistedVehicle in ipairs(whitelist) do
         if string.lower(vehicleName) == string.lower(whitelistedVehicle) then
             return true
         end
@@ -13,20 +16,28 @@ local function isVehicleWhitelisted(vehicleName)
     return false
 end
 
-RegisterCommand(commandName, function(source, args, rawCommand)
+-- Command to toggle cargo doors
+RegisterCommand(doorCommandName, function(source, args, rawCommand)
     ToggleDoor()
 end, false)
 
+-- Command to toggle bomb bays
+RegisterCommand(bombBayCommandName, function(source, args, rawCommand)
+    BombBayControl()
+end, false)
+
+-- Function to display notifications
 function DisplayNotification(text)
     SetNotificationTextEntry("STRING")
     AddTextComponentString(text)
     DrawNotification(false, false)
 end
 
+-- Function to toggle cargo doors
 function ToggleDoor()
     local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
 
-    if DoesEntityExist(vehicle) and isVehicleWhitelisted(GetDisplayNameFromVehicleModel(GetEntityModel(vehicle))) then
+    if DoesEntityExist(vehicle) and isVehicleWhitelisted(GetDisplayNameFromVehicleModel(GetEntityModel(vehicle)), vehicleWhitelist) then
         SetVehicleDoorShut(vehicle, 2, false) -- Door 3
         SetVehicleDoorShut(vehicle, 3, false) -- Door 4
 
@@ -44,5 +55,27 @@ function ToggleDoor()
         DisplayNotification("Cargo doors toggled!") -- Opening/Closing Doors
     else
         DisplayNotification("You are not in a whitelisted vehicle.") -- Whitelist Messages
+    end
+end
+
+-- Function to control bomb bays
+function BombBayControl()
+    local playerPed = GetPlayerPed(-1)
+    if (IsPedSittingInAnyVehicle(playerPed)) then
+        local vehicle = GetVehiclePedIsIn(playerPed, false)
+
+            if isVehicleWhitelisted(GetDisplayNameFromVehicleModel(GetEntityModel(vehicle)), bombBayWhitelist) then
+                if AreBombBayDoorsOpen(vehicle, bombBayIndex) then
+                    CloseBombBayDoors(vehicle, bombBayIndex)
+                    DisplayNotification("Bomb bays closed!")
+                else
+                    OpenBombBayDoors(vehicle, bombBayIndex)
+                    DisplayNotification("Bomb bays opened!")
+                end
+            else
+                DisplayNotification("You are not in a whitelisted vehicle, or this vehicle does not have bomb bays.") -- Whitelist Messages
+            end
+    else
+        DisplayNotification("You are not in a vehicle.")
     end
 end
